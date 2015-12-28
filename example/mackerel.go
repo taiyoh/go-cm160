baedc53cc5c182530a9730ccee9dd71b1fbf8fea
taiyoh <sun.basix@gmail.com>
2015-12-28 21:37:27 +0900

    一日以上前のデータ送ってもしょうがないからパス

--- example/mackerel.go
+++ example/mackerel.go
@@ -21,15 +21,18 @@ func NewMkrClient(config MackerelConfig, name string) *MkrClient {
 }
 
 func (self *MkrClient) post(record *cm160.Record) {
-	t := time.Date(record.Year, time.Month(record.Month), record.Day, record.Hour, record.Minute, time.Now().Second(), 0, self.Loc)
-	err := self.Client.PostHostMetricValuesByHostID(self.HostId, []*mackerel.MetricValue{
-		&mackerel.MetricValue{
-			Name:  self.Name,
-			Time:  t.Unix(),
-			Value: record.Amps,
-		},
-	})
-	if err != nil {
-		log.Printf("mackerel post error: %#v", err)
+	now := time.Now()
+	t := time.Date(record.Year, time.Month(record.Month), record.Day, record.Hour, record.Minute, now.Second(), 0, self.Loc)
+	if dur := now.Sub(t); dur.Hours() < 24.0 {
+		err := self.Client.PostHostMetricValuesByHostID(self.HostId, []*mackerel.MetricValue{
+			&mackerel.MetricValue{
+				Name:  self.Name,
+				Time:  t.Unix(),
+				Value: record.Amps,
+			},
+		})
+		if err != nil {
+			log.Printf("mackerel post error: %#v", err)
+		}
 	}
 }
